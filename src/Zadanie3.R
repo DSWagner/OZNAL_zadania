162159740a3e7c7a58341707416c5e1bc638cd3b
Viktor Klima <viktorklima@yahoo.com>
2024-05-05 23:32:32 +0200

    zadanie 3 wip

--- src/Zadanie3.R
+++ src/Zadanie3.R
@@ -15,6 +15,8 @@ library(pROC)
 library(kernlab)
 library(stats)
 library(e1071)
+library(igraph)
+library(ggraph)
 
 
 # Setting the working directory to where the dataset is located
@@ -33,28 +35,42 @@ head(raw_data)
 
 
 # Renaming columns in the dataset to make them more descriptive
-names(raw_data)[names(raw_data) == "FAVC"] <- "FreqConsHighCalFood"
-names(raw_data)[names(raw_data) == "FCVC"] <- "FreqConsVegs"
-names(raw_data)[names(raw_data) == "NCP"] <- "NumMainMeals"
-names(raw_data)[names(raw_data) == "CAEC"] <- "ConsFoodBetwMeals"
-names(raw_data)[names(raw_data) == "CH2O"] <- "ConsWaterDaily"
-names(raw_data)[names(raw_data) == "CALC"] <- "ConsAlc"
-names(raw_data)[names(raw_data) == "SCC"] <- "CalsConsMon"
-names(raw_data)[names(raw_data) == "FAF"] <- "PhysActFreq"
-names(raw_data)[names(raw_data) == "TUE"] <- "TimeTechDev"
-names(raw_data)[names(raw_data) == "MTRANS"] <- "Trans"
+# names(raw_data)[names(raw_data) == "FAVC"] <- "FreqConsHighCalFood"
+# names(raw_data)[names(raw_data) == "FCVC"] <- "FreqConsVegs"
+# names(raw_data)[names(raw_data) == "NCP"] <- "NumMainMeals"
+# names(raw_data)[names(raw_data) == "CAEC"] <- "ConsFoodBetwMeals"
+# names(raw_data)[names(raw_data) == "CH2O"] <- "ConsWaterDaily"
+# names(raw_data)[names(raw_data) == "CALC"] <- "ConsAlc"
+# names(raw_data)[names(raw_data) == "SCC"] <- "CalsConsMon"
+# names(raw_data)[names(raw_data) == "FAF"] <- "PhysActFreq"
+# names(raw_data)[names(raw_data) == "TUE"] <- "TimeTechDev"
+# names(raw_data)[names(raw_data) == "MTRANS"] <- "Trans"
+# names(raw_data)[names(raw_data) == "family_history_with_overweight"] <- "ObesityFam"
 
 # Identifying missing values in dataset
-sum(is.na(raw_data))
-
-# Identifying missing values per column
-colSums(is.na(raw_data))
-
-# If removing outliers introduced NAs, remove those rows
-cleaned_data %<>% drop_na()
-
-# Removing duplicate rows from the dataset, keeping only unique rows
-cleaned_data <- cleaned_data %>% distinct()
+cleaned_data <- raw_data
 
 # Check data types of each column
+str(cleaned_data)
+
+cleaned_data$FAF <- round(cleaned_data$FAF)
+cleaned_data$FAF <- pmin(pmax(cleaned_data$FAF, 0), 3)
+cleaned_data$FCVC <- round(cleaned_data$FCVC)
+cleaned_data$FCVC <- pmin(pmax(cleaned_data$FCVC, 0), 3)
+
+# Create categorical factors from FAF and FCVC
+cleaned_data$FAF_category <- ifelse(cleaned_data$FAF == 0, 'No activity',
+                            ifelse(cleaned_data$FAF == 1, 'Low activity',
+                                   ifelse(cleaned_data$FAF == 2, 'Moderate activity', 'High activity')))
+
+cleaned_data$FCVC_category <- ifelse(cleaned_data$FCVC == 1, 'Low consumption',
+                             ifelse(cleaned_data$FCVC == 2, 'Moderate consumption', 'High consumption'))
+
+cleaned_data$Age_category <- ifelse(cleaned_data$Age >= 14 & cleaned_data$Age <= 21, 'Youth',
+                            ifelse(cleaned_data$Age >= 22 & cleaned_data$Age <= 30, 'Young Adults',
+                                   ifelse(cleaned_data$Age >= 31 & cleaned_data$Age <= 50, 'Middle-Aged Adults', 'Senior')))
+
+cleaned_data <- cleaned_data %>%
+  mutate(across(where(is.character), as.factor))
+
 str(cleaned_data)
